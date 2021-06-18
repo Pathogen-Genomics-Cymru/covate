a185300841f9574ca92de04ba46ecf40e5161db5
Anna Price <36504362+annacprice@users.noreply.github.com>
2021-06-18 16:50:48 +0100

    update grangercausality function

--- stats_tests.py
+++ stats_tests.py
@@ -1,7 +1,7 @@
 import pandas as pd
+import numpy as np
 from statsmodels.tsa.stattools import grangercausalitytests
 from utils import pairwise
-
 
 def runtests(timeseries, lineagelist, regionlist):
 
@@ -11,16 +11,17 @@ def runtests(timeseries, lineagelist, regionlist):
 def grangercausality(timeseries, lineagelist, regionlist):
 
     maxlag=14
+    test = 'ssr_chi2test'
 
     for loc1, loc2 in pairwise(regionlist):
         for lineage in lineagelist:
-           data = pd.concat([timeseries[lineage + '_' + loc1], timeseries[lineage + '_' +  loc2]], axis=1)
-           try:
-               test_result = grangercausalitytests(data, maxlag=maxlag, verbose=False)
-               p_values = [test_result[i+1][0][test][1] for i in range(maxlag)]
-               min_p_value = np.min(p_values)
-               print (lineage)
-               print (loc2 + '->' + loc1)
-               print (min_p_value)
-           except:
-               print(lineage + " " + loc2 +  "-> " + loc1 + " failed")
+            data = pd.concat([timeseries[lineage + '_' + loc1], timeseries[lineage + '_' +  loc2]], axis=1)
+            try:
+                test_result = grangercausalitytests(data, maxlag=maxlag, verbose=False)
+                p_values = [test_result[i+1][0][test][1] for i in range(maxlag)]
+                min_p_value = np.min(p_values)
+                print (lineage)
+                print (loc2 + '->' + loc1)
+                print (min_p_value)
+            except:
+                print(lineage + " " + loc2 +  "-> " + loc1 + " failed")
