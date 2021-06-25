b856a389753b7987a30e6c163708315f50da3859
Anna Price <36504362+annacprice@users.noreply.github.com>
2021-06-25 16:35:17 +0100

    add appendline function

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from itertools import tee
 
 def pairwiseunique(iterable):
-    # return all unique pairwise combinations in a list, e.g. l=[i, j] -> (i, j)
+    """return all unique pairwise combinations in a list, e.g. l=[i, j] -> (i, j)"""
 
     i, j = tee(iterable)
     next(j, None)
@@ -10,7 +10,7 @@ def pairwiseunique(iterable):
 
 
 def pairwise(iterable):
-    # return all pairwise combinations in a list, e.g. l=[i, j] -> (i, j), (j, i)
+    """return all pairwise combinations in a list, e.g. l=[i, j] -> (i, j), (j, i)"""
 
     pairs = []
     for i in range(0,len(iterable)):
@@ -19,3 +19,13 @@ def pairwise(iterable):
                 pairs.append((iterable[i], iterable[j]))
 
     return pairs
+
+def appendline(filename, text):
+    """append text as new line at end of file"""
+
+    with open(filename, "a+") as infile:
+        infile.seek(0)
+        readtext = infile.read(10)
+        if len(readtext) > 0:
+            infile.write("\n")
+        infile.write(text)
