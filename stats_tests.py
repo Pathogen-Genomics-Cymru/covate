936a7bc796b0391f60f4e2d41d90ca071f6d13b7
Anna Price <36504362+annacprice@users.noreply.github.com>
2021-06-25 16:36:48 +0100

    write granger causality result to log file

--- stats_tests.py
+++ stats_tests.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from statsmodels.tsa.stattools import grangercausalitytests
-from utils import pairwise
+from utils import pairwise, appendline
 
 def runtests(timeseries, lineagelist, regionlist):
 
@@ -10,18 +10,19 @@ def runtests(timeseries, lineagelist, regionlist):
 
 def grangercausality(timeseries, lineagelist, regionlist):
 
-    maxlag=14
-    test = 'ssr_chi2test'
+    maxlag = 14
+    test = "ssr_chi2test"
 
     for loc1, loc2 in pairwise(regionlist):
         for lineage in lineagelist:
             data = pd.concat([timeseries[lineage + '_' + loc1], timeseries[lineage + '_' +  loc2]], axis=1)
             try:
                 test_result = grangercausalitytests(data, maxlag=maxlag, verbose=False)
-                p_values = [test_result[i+1][0][test][1] for i in range(maxlag)]
+                p_values = [test_result[lag+1][0][test][1] for lag in range(maxlag)]
                 min_p_value = np.min(p_values)
-                print (lineage)
-                print (loc2 + '->' + loc1)
-                print (min_p_value)
+                filename = str(lineage) + '_log.txt'
+                appendtext = 'Granger causality test result for ' + str(loc2) + '->' + str(loc1) + "\n" + 'min p-value = ' + str(min_p_value)
+                appendline(filename, appendtext)
             except:
-                print(lineage + " " + loc2 +  "-> " + loc1 + " failed")
+                appendtext = 'Granger causality test for ' + str(loc2) +  '-> ' + str(loc1) + ' failed'
+                appendline(filename, appendtext)
