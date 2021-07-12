9dbb38755684e35fbd481fbe1d9e665a8eff04e0
Anna Price <36504362+annacprice@users.noreply.github.com>
2021-07-13 00:04:19 +0100

    modify tests for model and parameter selection

--- build_model.py
+++ build_model.py
@@ -11,72 +11,88 @@ from utils import appendline, pairwise
 def buildmodel(timeseries, lineagelist, regionlist):
 
     maxlag=14
-    alpha = 0.05
     nsteps=14
+    alpha = 0.05
 
     for lineage in lineagelist:
+
+        # set log file
+        filename = str(lineage) + '_log.txt'
 
         # filter timeseries by lineage
         data = timeseries.filter(like=lineage)
 
+        # set index freq
+        data = data.asfreq('d')
+
+        # build training and testing datasets
+        X_train, X_test = data[0:-nsteps], data[-nsteps:]
+
         # get basic information on timeseries
-        checkdistribution(data, lineage, alpha)
+        checkdistribution(X_train, lineage, alpha, filename)
 
         # plot the autocorrelation
-        plotautocorr(data, lineage, maxlag)
+        plotautocorr(X_train, lineage, maxlag)
 
         # check for granger causality
-        grangercausality(data, lineage, regionlist, maxlag, alpha)
+        grangercausality(X_train, lineage, regionlist, maxlag, alpha, filename)
+
+        # find lag order
+        lag = lagorder(X_train, lineage, maxlag, filename)
 
         # record deterministic terms for cointegration
         VECMdeterm = ''
 
-        # first check cointegration for constant term and linear trend
+        # first check cointegration for constant term and linear trend, note if VECMdeterm='colo' then coint_count for 'lo' is selected
         for determ in range(0, 2):
 
-            runVECM = cointegration(timeseries, lineage, regionlist, maxlag, determ)
+            runVECM, coint_count = cointegration(X_train, lineage, regionlist, lag, determ, filename)
 
             VECMdeterm+= str(runVECM)
 
         # if no constant or linear determ then check cointegration for no determ
         if not VECMdeterm:
 
-            runVECM = cointegration(data, lineage, regionlist, maxlag, -1)
+            runVECM, coint_count = cointegration(X_train, lineage, regionlist, lag, -1, filename)
 
         # if lineage has cointegration, then run VECM
         if VECMdeterm:
 
-            vectorErrorCorr(data, lineage, VECMdeterm, regionlist, nsteps)
+            appendline(filename, 'Lineage has cointegration => Run VECM')
+
+            vectorErrorCorr(X_train, X_test, lineage, VECMdeterm, lag, coint_count, regionlist, nsteps, alpha, filename)
 
         # else check for stationarity and difference then run VAR
         else:
 
-            adf_result = adfullertest(data, lineagelist, alpha)
+            adf_result = adfullertest(X_train, lineagelist, alpha, filename)
 
             # record whether no diff, first diff or second diff
             VARdiff = 'none'
 
             # first diff and recalculate ADF
             if False in adf_result:
-                first_diff = data.diff().dropna()
-                adf_result = adfullertest(first_diff, lineagelist, alpha)
+
+                X_train = X_train.diff().dropna()
+
+                adf_result = adfullertest(X_train, lineagelist, alpha, filename)
+
                 VARdiff = 'first'
-                data = first_diff
 
             # second diff
             if False in adf_result:
-                second_diff = first_diff.diff().dropna()
-                adf_result = adfullertest(second_diff, lineagelist, alpha)
+
+                X_train = X_train.diff().dropna()
+
+                adf_result = adfullertest(X_train, lineagelist, alpha, filename)
+
                 VARdiff = 'second'
-                data = second_diff
 
 
-def checkdistribution(data, lineage, alpha):
+def checkdistribution(X_train, lineage, alpha, filename):
     """Get information about distribution"""
 
-    #data.reset_index(drop=True, inplace=True)
-    filename = str(lineage) + '_log.txt'
-    for name, col in data.iteritems():
+    for name, col in X_train.iteritems():
         stat, pvalue = stats.normaltest(col)
         appendline(filename, 'Information on distribution for ' + str(name))
         appendline(filename, 'Stat = ' + str(round(stat, 4)))
@@ -91,68 +107,75 @@ def checkdistribution(data, lineage, alpha):
         appendline(filename, 'Skewness = ' + str(round(stat_skew, 4)))
 
 
-def plotautocorr(data, lineage, maxlag):
+def plotautocorr(X_train, lineage, maxlag):
     """Plot autocorrelation"""
 
-    #data.reset_index(drop=True, inplace=True)
-    for name, col in data.iteritems():
+    for name, col in X_train.iteritems():
         plot_acf(col, lags=maxlag)
         plt.title('ACF for ' + str(name))
         plt.savefig(name + '_ACF.png')
         plt.clf()
 
-def grangercausality(data, lineage, regionlist, maxlag, alpha):
+
+def grangercausality(X_train,  lineage, regionlist, maxlag, alpha, filename):
     """Check for Granger Causality"""
 
     test = "ssr_chi2test"
 
     for loc1, loc2 in pairwise(regionlist):
-        data = pd.concat([data[lineage + '_' + loc1], data[lineage + '_' +  loc2]], axis=1)
+        data = pd.concat([X_train[lineage + '_' + loc1], X_train[lineage + '_' +  loc2]], axis=1)
         try:
             test_result = grangercausalitytests(data, maxlag=maxlag, verbose=False)
             p_values = [test_result[lag+1][0][test][1:3] for lag in range(maxlag)]
             min_p_value = min(p_values, key=lambda x: x[0])
-            filename = str(lineage) + '_log.txt'
-            appendline(filename, 'Granger causality test result for ' + str(loc2) + '->' + str(loc1) + "\n" + '(min p-value, lag) = ' + str(min_p_value) + ' =>  ' + str(min_p_value[0] <= alpha))
+            appendline(filename, 'Granger causality test result for ' + str(loc2) + '->' + str(loc1) + "\n" + 'minimum p-value = ' + str(round(min_p_value[0], 4)) + ' =>  ' + str(min_p_value[0] <= alpha))
         except:
             appendline(filename, 'Granger causality test for ' + str(loc2) +  '-> ' + str(loc1) + ' failed')
 
 
-def cointegration(timeseries, lineage, regionlist, maxlag, determ):
+def lagorder(X_train, lineage, maxlag, filename):
+    """Use VAR to select lag order"""
+
+    varmodel = VAR(endog=X_train)
+    lagorder = varmodel.select_order(maxlag)
+    lag = int(lagorder.aic)
+
+    appendline(filename, lagorder.summary().as_text())
+
+    return lag
+
+
+def cointegration(X_train, lineage, regionlist, lag, determ, filename):
     """Perform Johanson's Cointegration Test"""
 
     runVECM = ''
 
-    data = timeseries.filter(like=lineage)
-    data.reset_index(drop=True, inplace=True)
-    filename = str(lineage) + '_log.txt'
-    appendline(filename, 'Lags with Cointegration for ' + str(determ))
-    for lag in range(maxlag):
-        d = {'0.90':0, '0.95':1, '0.99':2}
+    d = {'0.90':0, '0.95':1, '0.99':2}
 
-        out = coint_johansen(data, determ, lag)
-        out_traces = out.lr1
-        out_cvts = out.cvt[:, d[str(0.95)]]
+    out = coint_johansen(X_train, determ, lag)
+    out_traces = out.lr1
+    out_cvts = out.cvt[:, d[str(0.95)]]
 
-        for col, trace, cvt in zip(data.columns, out_traces, out_cvts):
-            if trace > cvt:
-                appendline(filename, str(col) + ' lag= ' + str(lag))
-                if determ == 0:
-                    runVECM = 'co'
-                elif determ == 1:
-                    runVECM = 'lo'
-                elif determ == -1:
-                    runVECM = 'nc'
+    count_coint=0
+    for col, trace, cvt in zip(X_train.columns, out_traces, out_cvts):
+        if trace > cvt:
+            count_coint +=1
+            if determ == 0:
+                runVECM = 'co'
+            elif determ == 1:
+                runVECM = 'lo'
+            elif determ == -1:
+                runVECM = 'nc'
+    appendline(filename, 'Cointegration rank for ' + str(determ) + ' at lag ' + str(lag) + ' = ' + str(count_coint))
 
-    return runVECM
+    return runVECM, count_coint
 
 
-def adfullertest(timeseries, lineage, alpha):
+def adfullertest(X_train, lineage, alpha, filename):
     """Perform ADFuller to test for Stationarity"""
 
     adf_result = []
-    filename = str(lineage) + '_log.txt'
-    for name, col in timeseries.iteritems():
+    for name, col in X_train.iteritems():
         stat = adfuller(col, autolag ="AIC")
         out = {'test_statistic':round(stat[0], 4), 'pvalue':round(stat[1], 4), 'n_lags':round(stat[2], 4), 'n_obs':stat[3]}
         p_value = out['pvalue']
@@ -171,32 +194,22 @@ def adfullertest(timeseries, lineage, alpha):
     return adf_result
 
 
-def vectorErrorCorr(data, lineage, determ, regionlist, nsteps):
+def vectorErrorCorr(X_train, X_test, lineage, VECMdeterm, lag, coint_count, regionlist, nsteps, alpha, filename):
     """ Build VECM model"""
 
-    data = data.asfreq('d')
+    # minus 1 from lag for VECM
+    if lag != 0:
+        lag = int(lag) - 1
 
-    nobs = nsteps
-    X_train, X_test = data[0:-nobs], data[-nobs:]
-
-    varmodel = VAR(endog=X_train)
-    lagorder = varmodel.select_order(14, trend='c')
-    lag = int(lagorder.aic) - 1
-
-    vecm_rank = select_coint_rank(X_train, det_order = 1, k_ar_diff = lag, method = 'trace', signif=0.05)
-
-    vecm = VECM(endog = X_train, k_ar_diff = lag, coint_rank = vecm_rank.rank, deterministic = determ)
+    vecm = VECM(endog = X_train, k_ar_diff = lag, coint_rank = coint_count, deterministic = VECMdeterm)
 
     vecm_fit = vecm.fit()
     vecm_fit.predict(steps=nsteps)
 
-    forecast, lower, upper = vecm_fit.predict(nsteps, 0.05)
+    forecast, lower, upper = vecm_fit.predict(nsteps, alpha)
 
     pred = (pd.DataFrame(forecast.round(0), index=X_test.index, columns=X_test.columns))
 
-    filename = str(lineage) + '_log.txt'
-    appendline(filename, lagorder.summary().as_text())
-    appendline(filename, vecm_rank.summary().as_text())
     appendline(filename, vecm_fit.summary().as_text())
 
     for region in regionlist:
@@ -204,5 +217,8 @@ def vectorErrorCorr(data, lineage, determ, regionlist, nsteps):
         plt.plot(X_test[lineage + '_' + region], color='b',  label='actual')
         plt.title('VECM Predicted and Actual Time series for ' +  lineage + ' for ' + region)
         plt.legend(loc="upper left")
+        plt.locator_params(axis="y", integer=True, tight=True)
+        plt.xticks(rotation=45)
+        plt.tight_layout()
         plt.savefig(lineage + '_' + region + '_VECM.png')
         plt.clf()
